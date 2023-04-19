d97b301ddc05e12da11fbd435ecadf65bd336d2a
hackingyseguridad <antonio_taboada@hotmail.es>
2023-04-19 14:20:58 +0200

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 echo 
 echo "Instalando ... "
+setxkbmap es sundeadkeys
+update-locale LANG=es_ES.UTF-8 LANGUAGE
+localectl set-locale LANG=es_ES.UTF-8
+sudo localectl set-locale LC_TIME=es_ES.UTF-8
+sudo localectl set-locale LC_TIME=es_ES.UTF-8
+sudo locale-gen es_ES.UTF-8
+update-locale LANG=es_ES.UTF-8
 cp keyboard /etc/default/
 cp locale /etc/default/
 echo "#"
