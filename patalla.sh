714daef018a78574ececc4b0a4cdfd923d8c4c9f
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-02 13:50:39 +0100

    Update patalla.sh

--- patalla.sh
+++ patalla.sh
@@ -5,7 +5,16 @@
  # desde modo X  Xfce4
 
 xset s off
+xset q | grep -A 1 "DPMS"
 xset -dpms
-xset s noblank
+xset s off
+sudo sh -c 'echo "setterm -blank 0 -powerdown 0" >> /etc/rc.local'
+cat /etc/rc.local
+reboot
+vim /etc/kbd/config
+echo $XDG_CURRENT_DESKTOP
+export DISPLAY=:0\nxfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0\nxfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-off -s 0
+xset s off
+xset s noblack
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false
