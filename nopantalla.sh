b11febea7f9b60f0cc1e2cda7f2a46636898639e
hackingyseguridad <antonio_taboada@hotmail.es>
2025-12-03 19:02:47 +0100

    Update and rename patalla.sh to nopantalla.sh

--- nopantalla.sh
+++ nopantalla.sh
@@ -2,19 +2,26 @@
 # Sin salvapantalla, siempre activa!
  setvesablank 0
 
- # desde modo X  Xfce4
+# 
+
+# desde modo X  Xfce4
 
 xset s off
+xset s noblack
 xset q | grep -A 1 "DPMS"
 xset -dpms
 xset s off
+xset q
+
+#
+
 sudo sh -c 'echo "setterm -blank 0 -powerdown 0" >> /etc/rc.local'
 cat /etc/rc.local
-reboot
+
 vim /etc/kbd/config
 echo $XDG_CURRENT_DESKTOP
 export DISPLAY=:0\nxfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0\nxfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-on-ac-off -s 0
-xset s off
-xset s noblack
+
+
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0
 xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false
