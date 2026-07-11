6ba5a8651b42c231fb5d0afe44617909fea04918
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-11 22:51:38 +0200

    Update installchrome.sh with usage instructions
    Add instructions for running Chrome as a non-root user

--- installchrome.sh
+++ installchrome.sh
@@ -4,5 +4,7 @@
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
+echo "ejecutar desde consola y desde un usuario no root "
+
 google-chrome-stable --no-sandbox --user-data-dir
 
