463c6b3fb679724af55f7db6efddfccbb10514a7
hackingyseguridad <antonio_taboada@hotmail.es>
2026-07-12 08:18:33 +0200

    Update installchrome.sh

--- installchrome.sh
+++ installchrome.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# Simple Script en Bash Shell 1.0.x., para instalar y ejecutar el navegador Web Google Chrome en Linux Debian
 
-
+# Instalacion:
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
 
-echo "ejecutar desde consola y desde un usuario no root "
-
+# Elecutar
+echo 
+echo "ejecutar desde consola y desde un usuario no root .."
+echo
 google-chrome-stable --no-sandbox --user-data-dir
 
